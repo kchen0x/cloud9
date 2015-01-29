866b0ec637216c0c3ce3648ba6ae9852bb364b06
quentin-chen <quentin.chen@foxmail.com>
2015-01-29 13:30:06 +0000

    上传文件hello.py

---
+++ hello.py
@@ -0,0 +1,17 @@
+# this is the simplest python program
+# coding=utf-8
+
+print ("Hello world from Cloud9!")
+print("good!")
+def printMe(str):
+    """docstring for printMe"""
+    # TODO: write code...
+    print(str)
+    return 1
+
+if printMe("这是我的文档测试结果！"):
+    # TODO: write code...
+    printMe("succeed!")
+
+
+# click the 'Run' button at the top to start this application
